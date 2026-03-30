ec02eb187b3b32fb9f8216efa12a78478de275b7
keith-miquela <keithmiquela@gmail.com>
2026-03-29 22:56:11 -0400

    fix: dfs.c

--- dfs.c
+++ dfs.c
@@ -1,78 +1,95 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <dirent.h>
 #include "data_types.h"
 #include "queue.h"
 
-static char check_path(const char *path){
+static int check_path(const char *path) {
     struct stat path_stat;
-    if(stat(path, &path_stat) != 0){
-        perror("%s is not a file/directory");
+    if (stat(path, &path_stat) != 0) {
+        perror(path); 
         return -1;
     }
-    
-    if (S_ISREG(path_stat.st_mode)) {
-        // is a file
-        return 0;
-    }
-    else if(S_ISDIR(path_stat.st_mode)){
-        // is a dir
-        return 1;
-    }
+
+    if (S_ISREG(path_stat.st_mode)) return 0;
+    if (S_ISDIR(path_stat.st_mode)) return 1;
+    return -1;  
 }
 
-void *dfs(char **argv){
-    FileNode *fileHead = (FileNode *)malloc(sizeof(FileNode));
+FileNode *dfs(char **argv) {
+    FileNode *fileHead = malloc(sizeof(FileNode));
     fileHead->filePath = NULL;
     fileHead->wordList = NULL;
     fileHead->word_count = 0;
     fileHead->next = NULL;
 
-	char err = 0;
+    int err = 0;
     Queue *q = init();
 
-    char *temp = *argv;
-    while(temp){
-        enqueue(q, temp);
-        temp +=1;
+    char **temp = argv;
+    while (*temp) {
+        enqueue(q, strdup(*temp));
+        temp++;
     }
 
-    if(q->size == 0){
+    if (q->size == 0) {
         free(q);
+        free(fileHead);
         return NULL;
     }
 
     FileNode *tempNode = fileHead;
-
     char *currPath = dequeue(q);
-    while(currPath != NULL){
-        
-        int isDir = check_path(curr);
-        if(isDir == -1){
-            err = -1
-            continue;
-        }
-        else if(isDir == 0){
+
+    while (currPath != NULL) {
+        int isDir = check_path(currPath);  
+
+        if (isDir == -1) {
+            err = -1; 
+            free(currPath);
+        } else if (isDir == 0) {
             size_t len = strlen(currPath);
-            if(strcmp(currPath + len - 4), ".txt" != 0 || strcmp(currPath,".") == 0){
+            if (len < 4 || strcmp(currPath + len - 4, ".txt") != 0) {
                 free(currPath);
-                curr = dequeue(q);
-                continue;
+            } else {
+                FileNode *newNode = malloc(sizeof(FileNode));
+                newNode->filePath = currPath;
+                newNode->wordList = NULL;
+                newNode->word_count = 0;
+                newNode->next = NULL;
+                tempNode->next = newNode;
+                tempNode = newNode;
             }
+        } else {
+            DIR *dir = opendir(currPath);
+            if (dir == NULL) {
+                perror(currPath);
+                err = -1;
+                free(currPath);
+            } else {
+                struct dirent *entry;
+                while ((entry = readdir(dir)) != NULL) {
+                    if (strcmp(entry->d_name, ".") == 0 ||
+                        strcmp(entry->d_name, "..") == 0)
+                        continue;
 
-            FileNode *newNode = malloc(sizeof(FileNode));
-            newNode->filePath = curr;
-            newNode->wordList = NULL;
-            newNode->word_count = 0; //change this to actual word count after reading file
-            newNode->next = NULL;
-            tempNode = newNode;
-        }
-        else{
-            
+                    size_t pathLen = strlen(currPath) + strlen(entry->d_name) + 2;
+                    char *childPath = malloc(pathLen);
+                    snprintf(childPath, pathLen, "%s/%s", currPath, entry->d_name);
+                    enqueue(q, childPath);
+                }
+                closedir(dir);
+                free(currPath);
+            }
         }
 
-        curr = dequeue(q);
+        currPath = dequeue(q); 
     }
 
-    free_queue(q)
+    free_queue(q);
+
+    if (err) return NULL;
+    return fileHead;
 }
